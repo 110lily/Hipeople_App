f03aee6c77764c682f5821ea7ca18b9fdb3e8b54
AnMyungwoo94 <anmy121212@gmail.com>
2023-08-20 13:47:54 +0900

    [update] 노출된 server_key 숨기기

--- app/src/main/java/com/example/datingappkotlinproject/chat/fcm/Repo.kt
+++ app/src/main/java/com/example/datingappkotlinproject/chat/fcm/Repo.kt
@@ -1,10 +1,12 @@
 package com.example.datingappkotlinproject.chat.fcm
 
+import com.example.datingappkotlinproject.BuildConfig
+
 class Repo {
     companion object{
 
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAA6m2W6Ig:APA91bEKf4Hhq5EzA-BOWNHfQHlT7-27wCDeoGFlk6DrQSQa6I8kBdSeu4UCZ_xZjBIgXrJhiZjQa45p1w1ZoJBmVu9MxpMm527gGn2YGpy4Ykf8h3R0YTfPBAvU5f5BOwRDKo9PVYk_"
+        const val SERVER_KEY = BuildConfig.server_key
         const val CONTENT_TYPE = "application/json"
 
     }
