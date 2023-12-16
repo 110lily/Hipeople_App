9fde66c37e1d8052867240cc0901e35bc673413a
AnMyungwoo94 <anmy121212@gamil.com>
2023-12-16 19:34:01 +0900

    Add : Extensions 파일추가

--- app/src/main/java/com/myungwoo/datingappkotlinproject/Extensions.kt
+++ app/src/main/java/com/myungwoo/datingappkotlinproject/Extensions.kt
@@ -1,4 +1,13 @@
 package com.myungwoo.datingappkotlinproject
 
-class Extensions {
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+
+fun View.showSnackBar(message: String) {
+    Snackbar.make(
+        this,
+        message,
+        Snackbar.LENGTH_SHORT
+    ).show()
 }
