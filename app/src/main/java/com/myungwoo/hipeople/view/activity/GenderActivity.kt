9b6ea9597f6152468e03780c710bb9721ca80a1c
myungwoo <anmy121212@gmail.com>
2024-04-20 16:59:49 +0900

    Feat : 프로필 입력 화면에서 뒤로가기 버튼 수정(다이얼로그 추가)

--- app/src/main/java/com/myungwoo/hipeople/view/activity/GenderActivity.kt
+++ app/src/main/java/com/myungwoo/hipeople/view/activity/GenderActivity.kt
@@ -1,7 +1,12 @@
 package com.myungwoo.hipeople.view.activity
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.myungwoo.hipeople.R
 import com.myungwoo.hipeople.databinding.ActivityGenderBinding
@@ -10,10 +15,12 @@ class GenderActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGenderBinding
     private lateinit var gender: String
+    private var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        addOnBackPressedCallback()
         val id = intent.getStringExtra("id")
         val pw = intent.getStringExtra("pw")
         binding = ActivityGenderBinding.inflate(layoutInflater)
@@ -38,5 +45,32 @@ class GenderActivity : AppCompatActivity() {
             intent.putExtra("gender", gender)
             startActivity(intent)
         }
+    }
+
+    private fun addOnBackPressedCallback() {
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                AlertDialog.Builder(this@GenderActivity)
+                    .setTitle(R.string.gender_alert_dialog_title)
+                    .setMessage(R.string.gender_alert_dialog_message)
+                    .setPositiveButton(
+                        R.string.gender_alert_dialog_positive,
+                        object : DialogInterface.OnClickListener {
+                            override fun onClick(dialog: DialogInterface, which: Int) {
+                                handleOnBackCancelled()
+                            }
+                        })
+                    .setNegativeButton(
+                        R.string.gender_alert_dialog_negative,
+                        object : DialogInterface.OnClickListener {
+                            override fun onClick(dialog: DialogInterface, which: Int) {
+                                startActivity(Intent(this@GenderActivity, MainActivity::class.java))
+                            }
+                        })
+                    .create()
+                    .show()
+            }
+        }
+        this.onBackPressedDispatcher.addCallback(this, callback)
     }
 }
